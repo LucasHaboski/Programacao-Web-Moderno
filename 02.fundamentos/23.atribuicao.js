f768713e94337b56da05c7b3f6d0366c05d0ba76
Lucas Henrique Haboski da Silva <lucas.hhaboski@gmail.com>
2024-10-10 08:39:59 -0300

    Update 23.atribuicao.js

--- 02.fundamentos/23.atribuicao.js
+++ 02.fundamentos/23.atribuicao.js
@@ -7,7 +7,7 @@ console.log(`${a} + ${b-a} =`, b);
 b -= 4 // b = b - a
 console.log(`${b+4} - 4 =`, b)
 
-b += 2 // b = b * 2
+b *= 2 // b = b * 2
 console.log(`${b/2} * 2 =`, b)
 
 b /= 2 // b = b / 2
